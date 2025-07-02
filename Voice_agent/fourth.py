c432f8b4fcbd7279be7d0c0fd30fd125021330a8
HARSH BANSAL <113436817+harshbansal4351@users.noreply.github.com>
2025-07-02 14:00:14 +0530

    Delete Voice_agent/fourth.py

--- Voice_agent/fourth.py
+++
@@ -1,7 +0,0 @@
-def agent_decision(info, sentiment):
-    if info["Experience"] == "Fresher" and sentiment == "Positive":
-        return "Recommend for next round"
-    elif sentiment == "Negative":
-        return "Escalate to human HR for review"
-    else:
-        return "Put on hold"
